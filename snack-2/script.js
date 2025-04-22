cff060466d2140bb7cee977c3a2b522f5260b0cb
VoilaSecari <lifeaslaurafilip@gmail.com>
2025-04-22 13:30:15 +0200

    snack 2

--- snack-2/script.js
+++ snack-2/script.js
@@ -1,8 +1,11 @@
 const people = [
-  { name: 'Paolo', age: 35 },
-  { name: 'Giulia', age: 24 },
-  { name: 'Marco', age: 67 }
+  { name: "Paolo", age: 35 },
+  { name: "Giulia", age: 24 },
+  { name: "Marco", age: 67 },
 ];
 
 // Stampa in console tutti i nomi
 // Risultato: 'Paolo', 'Giulia', 'Marco'
+
+const peopleNames = people.map((person) => person.name);
+console.log(peopleNames);
